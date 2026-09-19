b6f7f7d53217c7befc3067a869e502a98ff15d7c
Sami Al Jaber Tasin <tasinjaber407@gmail.com>
2026-09-19 22:23:15 +0200

    chore: configure web linting

---
+++ apps/web/eslint.config.js
@@ -0,0 +1,1 @@
+import tsParser from "@typescript-eslint/parser"; export default [{ ignores:["dist"], files:["**/*.{ts,tsx}"], languageOptions:{parser:tsParser, parserOptions:{ecmaVersion:"latest",sourceType:"module",ecmaFeatures:{jsx:true}}}, rules:{}}];
